e6fd3778a3055e9be8ec0bc68f712e2f72c252b7
GrandNeedleWorker <46974401+GrandNeedleWorker@users.noreply.github.com>
2020-02-07 12:09:09 +0000

    Add files via upload
    Connecting the Frame to the Pool Class

--- Frame.java
+++ Frame.java
@@ -1,121 +1,137 @@
-
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Frame {
 
-	private static char [] frame;
-	private Pool pool = new Pool();
-	private Player player;
+    private static char [] frame;
+    private Pool pool = new Pool();
+    private Player player;
 
-	
-	public Frame() {
-		frame = new char[7];
-		frame = pool.assignToFrame();
-		
-	}
 
-	public char[] getFrame() {
-		return frame;
-	}
+    public Frame() {
+        frame = new char[7];
+        frame = pool.assignToFrame();
 
-	public void setFrame(char[] frame) {
-		frame = frame;
-	}
+    }
 
-	
-	
-	public static boolean IsFrameEmpty() {
-		
-		if(frame == null) {
-			System.out.println("Frame is Empty");
-			return false;
-		}
-		else {
-			return true;
-		}
-	}
+    public char[] getFrame() {
+        return frame;
+    }
 
-	
-	public static void Display() {
-		
-	//if frame is empty, access the pool
-		if(IsFrameEmpty() == false) {
-			
-			System.out.println("Frame is not full! Access the Pool.");
-			
-		}
-		else {
-			//else if frame has at least 1 tile, check amount 
-			
-			System.out.println("Frame has: "+ frame.length + " tile(s)");
-		}
-		
-		
-		//frame should never be over 7, exception
-		if(frame.length > 7) {
-			throw new RuntimeException("Frame is too big!");
-		}
-	
-		//if frame is full
-		if(frame.length == 7) {
-			System.out.println("Frame: ");
-			
-			//prints all tiles
-			for(int i = 0; i < frame.length; i++) {
-			System.out.print("[" + frame[i] + "]");
-			}
-			
-			System.out.print("\n");
-			
-			System.out.println("Do you want to swap (1)/ reset tiles (2) or play game (3)?");
-			Scanner r = new Scanner(System.in);
-			int removal = r.nextInt();
-			
-			if(removal == 1) {
-				System.out.println("you may remove tiles, select tile number");
-				
-				Scanner s = new Scanner(System.in);
-				int swap = s.nextInt();
-				
-				//swap(Frame, 2);
-			}
-			else if(removal == 2) {
-				System.out.println("you may reset tiles");
-				reset(frame);
-			}
-			else if(removal == 3) {
-				System.out.println("play game!");
-			}
-				
-		}
-		
-	}
-	
-	public void swap(char[] frameDisplay, int NumberOfLetter) {
-			
-		pool.swap(frameDisplay, NumberOfLetter);
-		
-	}
-	
-	public static void reset(char[] frameDisplay) {
-		
-		Arrays.fill(frame, ' ');
-		
-		for(int i = 0; i < frame.length; i++) {
-			System.out.print("[" + frame[i] + "]");
-			}
-		
-		//Pool pool;
-		
-	}
-	
-	public static void main(String[] args) {
-		
-		//Frame frame1 = new Frame();
-		//Arrays.toString(frame1);
-		Display();
+    public void setFrame(char[] frame) {
+        frame = frame;
+    }
 
-	}
+
+    /*public void getTilesOnFrame(){
+        frame = pool.assignToFrame();
+    }*/
+
+    public static boolean IsFrameEmpty() {
+
+        if(frame == null) {
+            System.out.println("Frame is Empty");
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+
+    public static void Display() {
+
+        //if frame is empty, access the pool
+        if(IsFrameEmpty() == false) {
+
+            System.out.println("Frame is not full! Access the Pool.");
+
+        }
+        else {
+            //else if frame has at least 1 tile, check amount
+
+            System.out.println("Frame has: "+ frame.length + " tile(s)");
+        }
+
+
+        //frame should never be over 7, exception
+        if(frame.length > 7) {
+            throw new RuntimeException("Frame is too big!");
+        }
+
+        //if frame is full
+        if(frame.length == 7) {
+            System.out.println("Frame: ");
+
+            //prints all tiles
+            for(int i = 0; i < frame.length; i++) {
+                System.out.print("[" + frame[i] + "]");
+            }
+
+            System.out.print("\n");
+
+            System.out.println("Do you want to swap (1)/ reset tiles (2) or play game (3)?");
+            Scanner r = new Scanner(System.in);
+            int removal = r.nextInt();
+
+            if(removal == 1) {
+                System.out.println("you may remove tiles, select tile number");
+
+                Scanner s = new Scanner(System.in);
+                int swap = s.nextInt();
+
+                //swap(Frame, 2);
+            }
+            else if(removal == 2) {
+                System.out.println("you may reset tiles");
+                reset(frame);
+            }
+            else if(removal == 3) {
+                System.out.println("play game!");
+            }
+
+        }
+
+    }
+
+    public void swap(int NumberOfLetter) {
+
+        pool.swap(NumberOfLetter);
+    }
+
+    public static void reset(char[] frameDisplay) {
+
+        Arrays.fill(frame, ' ');
+
+        for(int i = 0; i < frame.length; i++) {
+            System.out.print("[" + frame[i] + "]");
+        }
+
+        //Pool pool;
+
+    }
+
+    public String toString(){
+        String res = "";
+        for(char letter : frame){
+            res += letter + " ";
+        }
+        return res;
+    }
+
+    public static void main(String[] args)
+    {
+
+        Frame frame1 = new Frame();
+        //frame1.getTilesOnFrame();
+        System.out.println(frame1);
+        System.out.println(frame1.pool.getTotalTiles());
+        frame1.swap(4);
+        System.out.println(frame1);
+        //frame1.swap(3);
+        //System.out.println(frame1);
+        //Display();
+
+    }
 
 }
