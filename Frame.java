62b81f2f0c55238269a237ca5705fb13ac38d474
S-u-z <45173601+S-u-z@users.noreply.github.com>
2020-02-07 22:25:30 +0000

    Add files via upload

--- Frame.java
+++ Frame.java
@@ -1,19 +1,22 @@
+
 import java.util.Arrays;
 import java.util.Scanner;
 
-//Suzanne Byrne .
+// S-u-z    -> Suzanne Byrne -> 18321676
 
 public class Frame 
 {
 	private Pool pool = new Pool();;
     private char [] frame;
 
+    //frame constructor
     public Frame() {
         frame = new char[7];
         frame = pool.assignToFrame();
 
     }
 
+    //frame getters and setters
     public char[] getFrame() {
         return frame;
     }
@@ -22,6 +25,7 @@ public class Frame
         this.frame = frame;
     }
 
+    //function to check if frame is empty
     public  boolean IsFrameEmpty() {
 
         if(frame == null) {
@@ -33,22 +37,21 @@ public class Frame
         }
     }
 
-
+    //function to display frame
     public  void Display() {
 
         //if frame is empty, access the pool
         if(IsFrameEmpty() == false) {
-
+        	
             System.out.println("Frame is not full! Access the Pool.");
-
         }
+        
         else {
           //else if frame has at least 1 tile, check amount
-
           System.out.println("Frame has: "+ frame.length + " tile(s)");
         }
 
-
+        
         //frame should never be over 7, exception
         if(frame.length > 7) {
             throw new RuntimeException("Frame is too big!");
@@ -63,23 +66,33 @@ public class Frame
                 System.out.print("[" + frame[i] + "]");
             }
 
+            //whitespace
             System.out.print("\n");
 
+            //asks for user input to either swap reset or commence the game
+            //requires user to input an int value 
             System.out.println("Do you want to swap (1)/ reset tiles (2) or play game (3)?");
+            
+            //scans users input
             Scanner r = new Scanner(System.in);
             int removal = r.nextInt();
 
+            //if the input is 1, swap function is called
             if(removal == 1) {
                 System.out.println("you may remove tiles, select tile number");
 
+                //scans users input for an int value
+                //the int value represents the number of tiles to be swapped
                 Scanner s = new Scanner(System.in);
                 int swap = s.nextInt();
 
             }
+            //else if the input is 2, reset function is called
             else if(removal == 2) {
                 System.out.println("you may reset tiles");
                 reset(frame);
             }
+            //else if the input is 3, resume game play
             else if(removal == 3) {
                 System.out.println("play game!");
             }
@@ -88,22 +101,25 @@ public class Frame
 
     }
 
+    //swap function, swaps select number of tiles from the pool function
     public  void swap(Frame frame, int NumberOfLetter) {
 
         pool.swap(frame, NumberOfLetter);
     }
 
+    //reset function, empties the frame
     public  void reset(char[] frameDisplay) {
 
         Arrays.fill(frame, ' ');
 
+        //prints the now empty frame
         for(int i = 0; i < frame.length; i++) {
             System.out.print("[" + frame[i] + "]");
         }
 
-
     }
 
+    //returns the frame as a string
     public String toString(){
         String res = "";
         for(char letter : frame){
