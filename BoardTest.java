208d682443e799aadcec28a57751307e764b1590
daianamorjolic <49615616+daianamorjolic@users.noreply.github.com>
2020-02-23 19:10:22 +0000

    Add files via upload

--- BoardTest.java
+++ BoardTest.java
@@ -1,28 +1,29 @@
 
 public class BoardTest 
 {
-	public static void main(String[] args)
+	public static void main(String args[])
 	{
 		Board board = new Board();
+		Frame frame = new Frame();
+		
+		System.out.println(frame);
 		
 		//Board Display Test :
 		board.boardDisplay();
 		
-		//Input Test : 
-		board.getBoardInput();
-		board.boardDisplay();
-		board.getBoardInput();
-		board.boardDisplay();
+		//Scrabble game simulation test :
+		//Simulates a 2 round scrabble game where player can input tiles from the provided frame on screen .
+		for(int i = 0; i < 2; i++)
+		{
+			board.getBoardInput();
+			board.boardDisplay();
+		}
 		
-		//When using the 2 words : CAR DOG 
-		//the expected results are :
-		System.out.println("Expected Results : A and G ");
-		//board.tilePositionTestMethod();
+		//Testing tile position :
+		System.out.println("Tile on position row : 8 column : 8 is : " + board.getTile(8, 8));
 		
 		//Reset Test :
 		board.boardReset();
 		board.boardDisplay();
-
-		//
 	}
 }
